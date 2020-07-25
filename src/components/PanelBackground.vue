8ddf9ec88541325566b0f585b71d4c8e9907b302
wusatosi <26424577+wusatosi@users.noreply.github.com>
2020-07-25 15:52:01 +0000

    Finally got panels floating on top of canvas
    But this is only a prototype, I hacked out the css section,
    we should clean up css here afterwards

---
+++ src/components/PanelBackground.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="parent">
+    <h1>loaded</h1>
+    <canvas ref="canvas"/>
+  </div>
+</template>
+<script>
+import paper from "paper";
+
+export default {
+  name: "Panel-Background",
+  mounted() {
+    // Copyed from http://paperjs.org/tutorials/getting-started/using-javascript-directly/
+    paper.setup(this.$refs.canvas);
+		// Create a Paper.js Path to draw a line into it:
+		const path = new paper.Path();
+		// Give the stroke a color
+		path.strokeColor = 'black';
+		const start = new paper.Point(100, 100);
+		// Move to start and draw a line from there
+		path.moveTo(start);
+		// Note that the plus operator on Point objects does not work
+		// in JavaScript. Instead, we need to call the add() function:
+		path.lineTo(start.add([ 200, -50 ]));
+		// Draw the view now:
+		paper.view.draw();
+  }
+}
+</script>
+<style scoped>
+#parent {
+  height: 100%;
+}
+canvas {
+  height: 100%;
+  width: 100%;
+}
+</style>
--- src/views/Panels.vue
+++ src/views/Panels.vue
@@ -1,15 +1,30 @@
 <template>
-<div>
+<div id="panels">
+  <div id="panel-container">
     <panel/>
     <panel/>
+  </div>
+  <pb/>
 </div>
 </template>
 <script>
 import panel from '../components/Panel.vue'
+import pb from '../components/PanelBackground.vue'
 
 export default {
     components: {
-        panel
+        panel,
+        pb
     }
 }
 </script>
+<style scoped>
+#panels {
+  height: 100%;
+  width: 100%;
+}
+
+div {
+  display: inline;
+}
+</style>
